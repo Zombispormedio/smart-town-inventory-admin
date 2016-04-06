dd70312f42acb03985bd2ca254613029028dd35c
Xavier Serrano <zombispormedio007@gmail.com>
2016-04-06 15:51:59 +0200

    console.logs

--- public/app/components/filters/format_filters.js
+++ public/app/components/filters/format_filters.js
@@ -9,12 +9,10 @@ angular.module('Application').filter(
 angular.module('Application').filter(
     "search", function(){
         return function(items,field, searchObject) {
-            
             var text=searchObject.text;
             var result=items;
             if(items && text!==""){
                 var regex=new RegExp("^"+text.toLowerCase(), "g");
-                console.log(regex)
                 result=items.filter(function(a){
                     var elem=a[field].toLowerCase();
                     var test=regex.test(elem);
@@ -22,8 +20,6 @@ angular.module('Application').filter(
                     return test;
                 });
             }
-            console.log(result);
-
             return result;
         }}
 )
