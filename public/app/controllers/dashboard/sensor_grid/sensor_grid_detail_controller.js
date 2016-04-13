1e49fb57cd6ee26334f6a5404b1c93382f490053
Xavier Serrano <zombispormedio007@gmail.com>
2016-04-13 15:22:45 +0200

    secret

--- public/app/controllers/dashboard/sensor_grid/sensor_grid_detail_controller.js
+++ public/app/controllers/dashboard/sensor_grid/sensor_grid_detail_controller.js
@@ -21,9 +21,33 @@ angular.module('Application')
     $scope.editable={
         communication_center:false
     }
-    $scope.showSecret="password"
+    $scope.show={
+        secret:"password"
+    };
     
-    $scope.clickHandler = function (text) {
+    $scope.showSecret=function(){
+        $scope.show.secret="text";
+    }
+    
+    
+     $scope.reloadSecret=function(){
+        SensorGridService.Secret().reload({id:sensor_grid_id}, RequestService.Data(function(data){
+            $scope.sensor_grid=data;
+              $scope.show.secret="password";
+        }), RequestService.Error());
+    }
+        
+     
+     $scope.changeCommunicationCenter=function(){
+          var result=_.pick($scope.sensor_grid, ["device_name", "description"]);
+
+        SensorGridService.CommunicationCenter().set({id:sensor_grid_id},result, RequestService.Data(function(data){
+            $scope.sensor_grid=data;
+             $scope.editable.communication_center=false;
+        }), RequestService.Error());
+     }
+
+    $scope.copyToClipboard = function (text) {
         clipboard.copyText(text);
     };
 
